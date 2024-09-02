9421f441ef761af9ecdf3064a05cb3b757ed7f20
naolgithub <naolcovered@gmail.com>
2024-09-02 14:56:19 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,69 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Overlay Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void startOverlay() async {
+    if (await FlutterOverlayWindow.isActive()) {
+      FlutterOverlayWindow.closeOverlay();
+    } else {
+      await FlutterOverlayWindow.showOverlay(
+        enableDrag: true,
+        //  height: 500,
+        // width: 500,
+        height: 500,
+        width: 200,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: GestureDetector(
+          onTap: () {},
+          child: Text(widget.title),
+        ),
+      ),
       body: Center(
-        child: Text("Main thread"),
+        child: ElevatedButton(
+          onPressed: () {
+            startOverlay();
+          },
+          child: const Text("Start"),
+        ),
       ),
     );
   }
 }
+
 
 // overlay entry point
 @pragma("vm:entry-point")
